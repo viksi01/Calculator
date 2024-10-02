0d6d29fec6357a1e5b031ae3eb8a28f8664d1efb
viksi01 <viktoriia.piatnytska.ri.2022@lpnu.ua>
2024-10-02 23:37:00 +0300

    functions fixed

--- functions.py
+++ functions.py
@@ -1,36 +1,40 @@
 import math
 
+def check_floats(*operands):
+    for operand in operands:
+        if not isinstance(operand, float):
+            raise ValueError("All operands must be float numbers.")
+        
+def addition(first_operand, second_operand):
+    check_floats(first_operand, second_operand)
+    return first_operand + second_operand
 
-def addition (num1, num2):
- result = num1 + num2
- return result 
+def substraction(first_operand, second_operand):
+    check_floats(first_operand, second_operand) 
+    return first_operand - second_operand
 
-def substraction (num1, num2):
-   result = num1 - num2
-   return result 
+def multiplication (first_operand, second_operand):
+    check_floats(first_operand, second_operand) 
+    return first_operand * second_operand
 
-def multiplication (num1, num2):
-   result = num1 * num2
-   return result 
-
-def division (num1, num2):
-   while num2 == 0:
+def division(first_operand, second_operand):
+    check_floats(first_operand, second_operand)
+    while second_operand == 0:
         print('You cannot divide by zero')
-        num2 = float(input('Enter another second number: '))
+        second_operand = float(input('Enter another second number: '))
 
-   result = num1 / num2
-   return result
+    return first_operand / second_operand
 
-def power(num1, num2):
-    result = num1 ** num2
-    return result
+def power(first_operand, second_operand):
+    check_floats(first_operand, second_operand) 
+    return pow(first_operand, second_operand)
 
-def square_root(num):
-    if num < 0:
+def square_root(operand):
+    check_floats(operand) 
+    if operand < 0:
         return 'It is impossible to take the root of a negative number.'
-    result = math.sqrt(num)
-    return result
+    return math.sqrt(operand)
 
-def modulus(num1, num2):
-    result = num1 % num2
-    return result
+def modulus(first_operand, second_operand):
+    check_floats(first_operand, second_operand) 
+    return first_operand % second_operand
